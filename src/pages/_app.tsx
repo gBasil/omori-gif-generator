f5e1f0131721b3b0a5b9101f3efb137f56fd68a7
Basil <basil@gbasil.dev>
2022-10-15 13:50:16 -0600

    📈 Add selfhosted Umami analytics for my site only

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { GeistProvider, CssBaseline } from '@geist-ui/core';
 import Meta from '../components/Meta';
 
@@ -7,6 +8,16 @@ import '../styles/global.css';
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
+			<Head>
+				<script
+					async
+					defer
+					data-website-id='75086b50-13e1-4379-b5de-19a70d577cde'
+					src='https://speedcore.gbasil.dev/umami.js'
+					data-do-not-track='true'
+					data-domains='omori.gbasil.dev'
+				/>
+			</Head>
 			<Meta />
 			<GeistProvider>
 				<CssBaseline />
